d149160c35e6de4c79dec75ccd853d497c57c137
yuanyong <yuanyong129@outlook.com>
2024-10-23 18:00:12 +0800

    feat: 新增pwa支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+  
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+  
+      manifest: {
+        name: '空澄的博客',
+        short_name: '空澄的博客',
+        description: '空澄的博客 for pwa',
+        theme_color: '#ffffff',
+      },
+  
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+  
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
